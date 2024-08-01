fdaf084477cdc7ed0590506c30afa967f451bbdb
domantasdi <72785432+domantasdi@users.noreply.github.com>
2024-08-01 16:03:51 +0300

    Configs for the static github page

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.GITHUB_REPOSITORY
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
+    : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
